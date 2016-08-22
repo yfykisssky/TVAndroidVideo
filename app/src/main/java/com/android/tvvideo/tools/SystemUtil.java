b3062412a1b108dddf46a3efb5757397ce2b980c
yangfengyuan <yangfengyuan@dianping.com>
2016-08-22 16:12:12 +0800

    fix

--- app/src/main/java/com/android/tvvideo/tools/SystemUtil.java
+++ app/src/main/java/com/android/tvvideo/tools/SystemUtil.java
@@ -3,7 +3,6 @@ package com.android.tvvideo.tools;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
@@ -43,16 +42,6 @@ public class SystemUtil {
 
     }
 
-    public static String getMacAddress(Context context){
-
-        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-
-        WifiInfo info = wifi.getConnectionInfo();
-
-        return info.getMacAddress();
-
-    }
-
     //得到字符串型版本号
     public static String getVersion(Context context) {
         try {
@@ -64,13 +53,6 @@ public class SystemUtil {
             e.printStackTrace();
         }
         return null;
-    }
-
-    //得到数字型版本号
-    public static int getIntVersion(Context context){
-
-        return Integer.parseInt(getVersion(context).replace(".",""));
-
     }
 
     //得到系统的屏幕宽度
@@ -86,58 +68,6 @@ public class SystemUtil {
         return ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getHeight();
 
     }
-
-    //得到apk的签名字符数组信息
-    static Signature[] getSignature(Context context) {
-        String pkgname =context.getPackageName();
-        /** 通过包管理器获得指定包名包含签名的包信息 **/
-        PackageInfo packageInfo=null;
-        try {
-            packageInfo = context.getPackageManager().getPackageInfo(pkgname, PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        /******* 通过返回的包信息获得签名数组 *******/
-        Signature[] signatures = packageInfo.signatures;
-
-        return signatures;
-
-    }
-
-    //得到apk的签名字符串信息
-    public static String getSignStr(Context context) {
-
-        Signature[] signatures=getSignature(context);
-        /******* 循环遍历签名数组拼接应用签名 *******/
-        StringBuilder builder=new StringBuilder();
-        for (Signature signature : signatures) {
-            builder.append(signature.toCharsString());
-        }
-
-        /************** 得到应用签名 **************/
-        String signature = builder.toString();
-        return signature;
-    }
-
-    //得到apk的签名的hash取值
-    public static int getSignHash(Context context) {
-
-        final String packname = context.getPackageName();
-        PackageInfo packageInfo;
-        try {
-            packageInfo = context.getPackageManager().getPackageInfo(packname, PackageManager.GET_SIGNATURES);
-            Signature[] signs = packageInfo.signatures;
-            Signature sign = signs[0];
-            int code = sign.hashCode();
-            return code;
-        } catch (PackageManager.NameNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return 0;
-    }
-
 
     // 得到本机ip地址
     public static String getLocalHostIp()
